befee317e6445867b7dc87007791fe1c0f8a3bfd
Azayk <151768633+Azayk@users.noreply.github.com>
2024-03-14 21:01:34 +0700

    Add files via upload

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,42 @@
+﻿#include <stdio.h>
+
+void moveZerosToEnd(int arr[], int size) 
+{
+    // Проходим по массиву и переместите ненулевые элементы в начало
+    int count = 0; 
+        for (int i = 0; i < size; i++) 
+        {
+            if (arr[i] != 0) 
+            {
+                arr[count++] = arr[i];
+            }
+        }
+    
+    // Заполните оставшиеся позиции нулями
+    while (count < size) 
+    {
+        arr[count++] = 0;
+    }
+}
+
+int main()
+{
+    int arr[] = { 1, 0, 8, 0, 3, 0, 5, 0, 12, 0 };
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Before: ");
+    for (int i = 0; i < size; i++) 
+    {
+        printf("%d ", arr[i]);
+    }
+
+    moveZerosToEnd(arr, size);
+
+    printf("\nAfter: ");
+    for (int i = 0; i < size; i++) 
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
